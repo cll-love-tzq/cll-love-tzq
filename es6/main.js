798718d72fa262dde903899fe1132bb2648fdc19
lan-dell <lanruqing@hotmail.com>
2019-03-04 17:44:27 +0800

    count down v1.0

--- es6/main.js
+++ es6/main.js
@@ -1,4 +1,16 @@
-function test(){
-    let i = 0;
-    console.log(i);
+function count_down(){
+    let wedding_date = new Date('June 30, 2019');
+    let today = new Date();
+    const main = Date.parse(wedding_date) - Date.parse(today);
+    let ct = {
+        day:Math.floor(main/(1000 * 60 * 60 * 24)),
+        hour: Math.floor((main / (1000 * 60 * 60)) % 24),
+        minutes:Math.floor((main / 1000 / 60) % 60),
+        seconds:Math.floor((main / 1000) % 60)
+    }
+    console.log(main+'\n'+ct.day)
+    document.querySelector('#countDown').innerHTML = `距离婚礼还有${ct.day}天${ct.hour}小时${ct.minutes}分钟${ct.seconds}秒`
+}
+window.onload = ()=>{
+    setInterval(function(){count_down()},1000);
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 "use strict";
 
-function test() {
-  var i = 0;
-  console.log(i);
+function count_down() {
+  var wedding_date = new Date('June 30, 2019');
+  var today = new Date();
+  var main = Date.parse(wedding_date) - Date.parse(today);
+  var ct = {
+    day: Math.floor(main / (1000 * 60 * 60 * 24)),
+    hour: Math.floor(main / (1000 * 60 * 60) % 24),
+    minutes: Math.floor(main / 1000 / 60 % 60),
+    seconds: Math.floor(main / 1000 % 60)
+  };
+  console.log(main + '\n' + ct.day);
+  document.querySelector('#countDown').innerHTML = "\u8DDD\u79BB\u5A5A\u793C\u8FD8\u6709".concat(ct.day, "\u5929").concat(ct.hour, "\u5C0F\u65F6").concat(ct.minutes, "\u5206\u949F").concat(ct.seconds, "\u79D2");
 }
+
+window.onload = function () {
+  setInterval(function () {
+    count_down();
+  }, 1000);
+};
