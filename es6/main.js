a71f2d6e2a351c5e5c147f1d562c87570ad695b6
Lanruqing <lanruqing@hotmail.com>
2019-03-22 21:41:50 +0800

    big change

--- es6/main.js
+++ es6/main.js
@@ -19,41 +19,97 @@ function count_down(){
 }
 var wi = new Vue({
     el:"#wedding_invitaiton",
-    data:{
-        text:123,
+    data: {
+        text: 123,
         countDown: {
-            days:{
-              title:"Days",
-              value:0
-            },
-            hours:{
-              title:"Hours",
-              value:0
-            },
-            minutes: {
-              title:"Minutes",
-              value:0
-            },
-            seconds: {
-              title:"Seconds",
-              value:0
-            }
+          days: {
+            title: "Days",
+            value: 0
           },
-        couple_info:{
-            groom:{
-                title:"The Groom",
-                name:"兰如青",
-                phone:"15040501365",
-                avatar:'./../img/lrq.jpg'
-            },
-            bride:{
-                title:"The Bride",
-                name:"梅思雨",
-                phone:"13940943132",
-                avatar:'./../img/msy.jpg'
-            }
+          hours: {
+            title: "Hours",
+            value: 0
+          },
+          minutes: {
+            title: "Minutes",
+            value: 0
+          },
+          seconds: {
+            title: "Seconds",
+            value: 0
+          }
+        },
+        couple_info: {
+          groom: {
+            title: "The Groom",
+            name: "Lan Ruqing",
+            phone: "15040501365",
+            avatar: './../img/lrq.jpg'
+          },
+          bride: {
+            title: "The Bride",
+            name: "Mei Siyu",
+            phone: "13940943132",
+            avatar: './../img/msy.jpg'
+          }
+        },
+        timeline_nodes:[
+          {
+            time:"2010",
+            title:"we met at school",
+            icon:"icon-school"
+          },
+          {
+            time:"Nov 08, 2012",
+            title:"we fall in love",
+            icon:"icon-aiqingyuehui"
+          },
+          {
+            time:"2018",
+            title:"We ready to get married",
+            icon:"icon-svgmanylove"
+          },
+          {
+            time:"Feb 14, 2019",
+            title:"We got married",
+            icon:"icon-jiehunzhengshu"
+          },
+          {
+            time:"Jun 30, 2019",
+            title:"Our wedding",
+            icon:"icon-bhjwedding"
+          }
+        ],
+        gallery:[
+          {
+            title:"sea",
+            src:'./img/g_sea.jpg'
+          },
+          {
+            title:"park",
+            src:'./img/g_park.jpg'
+          },
+          {
+            title:"Formal",
+            src:'./img/g_formal.jpg'
+          },
+          {
+            title:"Valencia",
+            src:'./img/g_vcf.jpg'
+          },
+          {
+            title:"Old Street",
+            src:'./img/g_street.jpg'
+          }
+        ]
+      },
+      filters: {
+        uppercase: function (value) {
+          if (!value) return ''
+          value = value.toUpperCase();
+          return value
         }
-    },
+      },
     methods:{
         countDownFunc(){
             let wedding_date = new Date('June 30, 2019');
@@ -68,10 +124,48 @@ var wi = new Vue({
         _countDownFunc(){
             let that = this;
             setInterval(()=>{that.countDownFunc()},1000);
-        }
+        },
+        timeline(top){
+            let timeline_rows = Array.from(document.querySelectorAll('.timeline_row'));
+            timeline_rows.forEach((t,i)=>{
+              if(t.offsetTop < top - t.offsetHeight){
+                t.classList.add('flipInX')
+              }
+            })
+          },
+          choosePic(index){
+            let ctrls = Array.from(document.querySelectorAll('.gallery_ctrls'));
+            let pictures = Array.from(document.querySelectorAll('.photos'));
+            ctrls.forEach((c,i)=>{
+              c.classList.remove('active');
+            })
+            pictures.forEach((c,i)=>{
+              c.classList.remove('active');
+            })
+            ctrls[index].classList.add('active');
+            pictures[index].classList.add('active');
+          }
     },
     created(){
+        if(document.body.clientWidth <= 580){
+            document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 12 + 'px';
+            console.log('mobile')
+          }else if(document.body.clientWidth <= 1000){
+            document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 15 + 'px';
+            console.log('tablet')
+          }else{
+            document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 20 + 'px';
+            console.log('pc')
+          }
         document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 10 + 'px';
         this. _countDownFunc()
+        window.onscroll = function() {
+            var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            wi.timeline(scrollTop);
+            if(scrollTop > document.getElementById('dot1').offsetTop){
+              console.log(123)
+              document.getElementsByClassName('photos')[0].classList.add('active');
+            }
+          }
     },
 })
--- js/main.js
+++ js/main.js
@@ -40,16 +40,60 @@ var wi = new Vue({
     couple_info: {
       groom: {
         title: "The Groom",
-        name: "兰如青",
+        name: "Lan Ruqing",
         phone: "15040501365",
         avatar: './../img/lrq.jpg'
       },
       bride: {
         title: "The Bride",
-        name: "梅思雨",
+        name: "Mei Siyu",
         phone: "13940943132",
         avatar: './../img/msy.jpg'
       }
+    },
+    timeline_nodes: [{
+      time: "2010",
+      title: "we met at school",
+      icon: "icon-school"
+    }, {
+      time: "Nov 08, 2012",
+      title: "we fall in love",
+      icon: "icon-aiqingyuehui"
+    }, {
+      time: "2018",
+      title: "We ready to get married",
+      icon: "icon-svgmanylove"
+    }, {
+      time: "Feb 14, 2019",
+      title: "We got married",
+      icon: "icon-jiehunzhengshu"
+    }, {
+      time: "Jun 30, 2019",
+      title: "Our wedding",
+      icon: "icon-bhjwedding"
+    }],
+    gallery: [{
+      title: "sea",
+      src: './img/g_sea.jpg'
+    }, {
+      title: "park",
+      src: './img/g_park.jpg'
+    }, {
+      title: "Formal",
+      src: './img/g_formal.jpg'
+    }, {
+      title: "Valencia",
+      src: './img/g_vcf.jpg'
+    }, {
+      title: "Old Street",
+      src: './img/g_street.jpg'
+    }]
+  },
+  filters: {
+    uppercase: function uppercase(value) {
+      if (!value) return '';
+      value = value.toUpperCase();
+      return value;
     }
   },
   methods: {
@@ -68,11 +112,52 @@ var wi = new Vue({
       setInterval(function () {
         that.countDownFunc();
       }, 1000);
+    },
+    timeline: function timeline(top) {
+      var timeline_rows = Array.from(document.querySelectorAll('.timeline_row'));
+      timeline_rows.forEach(function (t, i) {
+        if (t.offsetTop < top - t.offsetHeight) {
+          t.classList.add('flipInX');
+        }
+      });
+    },
+    choosePic: function choosePic(index) {
+      var ctrls = Array.from(document.querySelectorAll('.gallery_ctrls'));
+      var pictures = Array.from(document.querySelectorAll('.photos'));
+      ctrls.forEach(function (c, i) {
+        c.classList.remove('active');
+      });
+      pictures.forEach(function (c, i) {
+        c.classList.remove('active');
+      });
+      ctrls[index].classList.add('active');
+      pictures[index].classList.add('active');
     }
   },
   created: function created() {
+    if (document.body.clientWidth <= 580) {
+      document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 12 + 'px';
+      console.log('mobile');
+    } else if (document.body.clientWidth <= 1000) {
+      document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 15 + 'px';
+      console.log('tablet');
+    } else {
+      document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 20 + 'px';
+      console.log('pc');
+    }
+
     document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth / 10 + 'px';
 
     this._countDownFunc();
+
+    window.onscroll = function () {
+      var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+      wi.timeline(scrollTop);
+
+      if (scrollTop > document.getElementById('dot1').offsetTop) {
+        console.log(123);
+        document.getElementsByClassName('photos')[0].classList.add('active');
+      }
+    };
   }
 });
